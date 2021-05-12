24298270264f001660c8d04b81dae48ec05a71b1
Anthony Salemo <ajssalemo@gmail.com>
2021-05-12 17:25:55 -0400

    corrected logic within the retry wrapper and added some additonal stdout

--- app.py
+++ app.py
@@ -18,19 +18,23 @@ logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 # This function implements a back off reconnection attempt with a maximum of 5 tries before throwing the exception
 # This function wraps all queries to catch disconnects and/or unable to connect to database errors
-def retry_with_backoff(f, attempts=5, backoff_in_seconds=1):
-    while attempts > 0:
-        attempts -= 1
+def retry_with_backoff(f):
+    attempts = 0
+    backoff_in_seconds = 1
+    while attempts < 5:
+        attempts += 1
         try:
             logging.info("Connecting to Postgres through SQLAlchemy..")
             return f()  # "break" if query was successful and return any results
         except SA.exc.DBAPIError as exc:
-            if attempts > 0 and exc.connection_invalidated:
-                logging.warning(
-                    f"Retry - trying to establish a connection. Attempt {attempts}"
-                )
+            if attempts < 5 and exc.connection_invalidated:
                 session.rollback()
                 sleep = backoff_in_seconds * 2 ** attempts + random.uniform(0, 1)
+                # Rounding the elapsed time to a whole number
+                elapsed_time = round(sleep)
+                logging.warning(
+                    f" Retry - trying to establish a connection. Attempt {attempts} - elapsed time: {elapsed_time} seconds"
+                )
                 time.sleep(sleep)
             else:
                 logging.error("Unable to connect to the database.")
